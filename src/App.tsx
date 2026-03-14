b9ddfe4a2ff901e65d2f559e90e2b15887c086e5
soundous <bsoundous112@gmail.com>
2026-03-14 10:33:28 +0100

    adding tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
+import "./index.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
